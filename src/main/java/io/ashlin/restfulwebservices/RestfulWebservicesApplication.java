2e4bbbc3d45363f2ab299c7e2b6bdd4795161fb9
AshlinRosemary <ashlinrosemary@gmail.com>
2023-09-27 15:44:23 +0400

    Added Spring-Doc-OpenAPI for Documentation

--- src/main/java/io/ashlin/restfulwebservices/RestfulWebservicesApplication.java
+++ src/main/java/io/ashlin/restfulwebservices/RestfulWebservicesApplication.java
@@ -1,11 +1,25 @@
 package io.ashlin.restfulwebservices;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "User Management App Documentation",
+				description="All rest-apis are documented here",
+				version="v1.0",
+				contact=@Contact(
+						name= "Ashlin",
+						email ="ashlinrosemary@gmail.com"
+				)
+		)
+)
 public class RestfulWebservicesApplication {
 
 	@Bean
